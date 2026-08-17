20545b846f4e2e9e6cd9f7f4076a81145f4cdee2
Nemanja <nemanja@dev63.com>
2026-08-18 01:59:06 +0200

    fix(cgo): update library flag to -lepplet

--- cgo_flags.go
+++ cgo_flags.go
@@ -2,7 +2,7 @@ package epplet
 
 /*
 #cgo freebsd CFLAGS: -I/usr/local/include
-#cgo freebsd LDFLAGS: -L/usr/local/lib -lEpplet -lX11 -lImlib2
-#cgo linux LDFLAGS: -lEpplet -lX11 -lImlib2
+#cgo freebsd LDFLAGS: -L/usr/local/lib -lepplet -lX11 -lImlib2
+#cgo linux LDFLAGS: -lepplet -lX11 -lImlib2
 */
 import "C"
