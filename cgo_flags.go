1b67fc038f22789ed5449d1b2a1c7b71f9b39fa2
Nemanja <nemanja@dev63.com>
2026-08-18 02:21:10 +0200

    fix(cgo): update LDFLAGS in cgo_flags.go with -lGL

--- cgo_flags.go
+++ cgo_flags.go
@@ -2,7 +2,7 @@ package epplet
 
 /*
 #cgo freebsd CFLAGS: -I/usr/local/include
-#cgo freebsd LDFLAGS: -L/usr/local/lib -lepplet -lepplet_glx -lX11 -lImlib2
-#cgo linux LDFLAGS: -lepplet -lepplet_glx -lX11 -lImlib2
+#cgo freebsd LDFLAGS: -L/usr/local/lib -lepplet -lepplet_glx -lX11 -lImlib2 -lGL
+#cgo linux LDFLAGS: -lepplet -lepplet_glx -lX11 -lImlib2 -lGL
 */
 import "C"
